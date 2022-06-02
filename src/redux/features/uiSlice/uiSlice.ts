2a9570ee322dbf09cce529f83587e0a892f439df
Cristian Bermúdez <ccbabcn@gmail.com>
2022-06-02 21:41:02 +0200

    implemented uiSlice loading on

---
+++ src/redux/features/uiSlice/uiSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UiState {
+  loading: boolean;
+}
+
+const initialState: UiState = {
+  loading: false,
+};
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: initialState,
+  reducers: {
+    loadingOn: (ui, action: PayloadAction<UiState>) => ({
+      ...action.payload,
+      loading: true,
+    }),
+  },
+});
+
+export const { loadingOn: loadingOnActionCreator } = uiSlice.actions;
+
+export default uiSlice.reducer;
