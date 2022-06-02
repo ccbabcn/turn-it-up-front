334e010c0ee8e86f24a20e8620a97369f4d4fa78
Cristian Bermúdez <ccbabcn@gmail.com>
2022-06-02 22:20:00 +0200

    implemented loadingOffActionCreator test

--- src/redux/features/uiSlice/uiSlice.test.ts
+++ src/redux/features/uiSlice/uiSlice.test.ts
@@ -1,4 +1,8 @@
-import uiReducer, { loadingOnActionCreator, UiState } from "./uiSlice";
+import uiReducer, {
+  loadingOffActionCreator,
+  loadingOnActionCreator,
+  UiState,
+} from "./uiSlice";
 
 describe("Given  uiReducer", () => {
   describe("When its invoked and the ui actual status is loading 'false'", () => {
@@ -12,6 +16,23 @@ describe("Given  uiReducer", () => {
       };
 
       const loadingAction = loadingOnActionCreator(uiActualState);
+      const loadingState = uiReducer(uiActualState, loadingAction);
+
+      expect(loadingState).toEqual(uiExpectedState);
+    });
+  });
+
+  describe("When it's invoked and the ui actual state is loading 'true'", () => {
+    test("Then it should set a new state with loading set to 'false'", () => {
+      const uiActualState: UiState = {
+        loading: true,
+      };
+
+      const uiExpectedState: UiState = {
+        loading: false,
+      };
+
+      const loadingAction = loadingOffActionCreator(uiActualState);
       const loadingState = uiReducer(uiActualState, loadingAction);
 
       expect(loadingState).toEqual(uiExpectedState);
