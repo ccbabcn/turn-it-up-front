043e052fc4b9a577b770894b96a43ac266af3d5c
Cristian Bermúdez <ccbabcn@gmail.com>
2022-06-04 19:50:19 +0200

    implemented basic structure of projectsSlice

---
+++ src/redux/features/projectsSlice/projectsSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ProjectsState } from "../../../types/ProjectsTypes";
+
+const initialState: ProjectsState = [];
+
+const projectsSlice = createSlice({
+  name: "projects",
+  initialState: initialState,
+  reducers: {
+    loadAllProjects: (projects, action: PayloadAction<ProjectsState>) => ({
+      ...action.payload,
+    }),
+  },
+});
+
+export const { loadAllProjects: loadAllProjectsActionCreator } =
+  projectsSlice.actions;
+
+export default projectsSlice.reducer;
