b3cfbe6c1a5da007e2f072de725d70a8bd537a51
Cristian Bermúdez <ccbabcn@gmail.com>
2022-06-04 22:31:41 +0200

    implemented loadAllProjectsThunk error  path test

--- src/redux/thunks/projectsThunks/projectsThunks.test.ts
+++ src/redux/thunks/projectsThunks/projectsThunks.test.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { mockProjects } from "../../../mocks/mockProjects/mockProjects";
 import { loadAllProjectsActionCreator } from "../../features/projectsSlice/projectsSlice";
+import {
+  loadingOffActionCreator,
+  UiState,
+} from "../../features/uiSlice/uiSlice";
 import { loadAllProjectsThunk } from "./projectsThunks";
 
 describe("Given a projectsThunks", () => {
@@ -10,6 +14,22 @@ describe("Given a projectsThunks", () => {
       const expectedAction = loadAllProjectsActionCreator(mockProjects);
       axios.get = jest.fn().mockResolvedValueOnce({
         data: { projects: mockProjects, status: 200 },
+      });
+
+      const thunk = await loadAllProjectsThunk();
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+  describe("When loadAllProjectsThunk it's invoked but there's no response", () => {
+    test("Then it should call dispatch with loadAllProjectsActionCreator whit that list", async () => {
+      const dispatch = jest.fn();
+      const mockUiState: UiState = { loading: false };
+
+      const expectedAction = loadingOffActionCreator(mockUiState);
+      axios.get = jest.fn().mockResolvedValueOnce({
+        data: { projects: {}, status: 404 },
       });
 
       const thunk = await loadAllProjectsThunk();
