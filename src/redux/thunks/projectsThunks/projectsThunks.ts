468843a1e66e5075497717e9d4039771f27ffe72
Cristian Bermúdez <ccbabcn@gmail.com>
2022-06-07 15:16:51 +0200

    implemented and tested created project thunk

--- src/redux/thunks/projectsThunks/projectsThunks.ts
+++ src/redux/thunks/projectsThunks/projectsThunks.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosResponse } from "axios";
 import { correctAction, wrongAction } from "../../../modals/modals";
+import { IProject } from "../../../types/ProjectsTypes";
 import {
+  createProjectActionCreator,
   deleteProjectActionCreator,
   loadAllProjectsActionCreator,
 } from "../../features/projectsSlice/projectsSlice";
@@ -55,6 +57,30 @@ export const deleteProjectThunk =
       }
     } catch (error) {
       wrongAction("Something went wrong deleting the project");
+    }
+    dispatch(loadingOffActionCreator({ loading: false }));
+  };
+
+export const createProjectThunk =
+  (newProject: IProject) => async (dispatch: AppDispatch) => {
+    try {
+      const token = localStorage.getItem("token");
+      const url = process.env.REACT_APP_API_URL as string;
+      const endPoint: string = `projects/create`;
+      dispatch(loadingOnActionCreator({ loading: true }));
+
+      const {
+        data: { project },
+        status,
+      }: AxiosResponse = await axios.post(`${url}${endPoint}`, newProject, {
+        headers: { authorization: `Bearer ${token}` },
+      });
+      if (status === 201) {
+        dispatch(createProjectActionCreator(project));
+        correctAction("Project created correctly");
+      }
+    } catch (error) {
+      wrongAction("Something went wrong creating the project");
     }
     dispatch(loadingOffActionCreator({ loading: false }));
   };
