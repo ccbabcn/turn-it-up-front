0f5ed7d0901bb995f198400fb0d4ce1da7861e84
Cristian Bermúdez <ccbabcn@gmail.com>
2022-06-09 15:17:22 +0200

    improved public and private route functionallity

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import jwtDecode from "jwt-decode";
+import { useEffect } from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import PublicRoute from "./components/PublicRoute/PublicRoute";
 import Spinner from "./components/Spinner/Spinner";
 import CreateProjectPage from "./pages/CreateProjectPage/CreateProjectPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
@@ -9,24 +12,50 @@ import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import UserProjectsPage from "./pages/UserProjectsPage/UserProjectsPage";
 import { spinnerState } from "./redux/features/uiSlice/uiSlice";
-import { useAppSelector } from "./redux/hooks";
+import { userLoginActionCreator } from "./redux/features/userSlice/userSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { UserLoggedIn } from "./types/UserTypes";
 
 function App(): JSX.Element {
+  const token = localStorage.getItem("token");
+  const dispatch = useAppDispatch();
   const spinnerIsVisible = useAppSelector(spinnerState);
   const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (token as string) {
+      const user: UserLoggedIn = jwtDecode(token as string);
+
+      dispatch(userLoginActionCreator(user));
+    }
+  }, [dispatch, token]);
 
   return (
     <>
       <Spinner visible={spinnerIsVisible} />
       <Header />
-      {pathname.includes("projects") && <Navigation />}
-      {pathname.includes("create") && <Navigation />}
+      {pathname.includes("project") && <Navigation />}
 
       <Routes>
+        <Route path="*" element={<Navigate to="/login" />} />
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
 
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <RegisterPage />
+            </PublicRoute>
+          }
+        />
         <Route
           path="/projects"
           element={
@@ -44,7 +73,7 @@ function App(): JSX.Element {
           }
         />
         <Route
-          path="/create"
+          path="/create-project"
           element={
             <PrivateRoute>
               <CreateProjectPage />
